e53a1db9aa9582702781b4a05ba522772fa585f6
Peter Szalay <peterszalay@users.noreply.github.com>
2020-04-22 23:47:08 +0200

    Server exercise 07: Adding concurrency with go serve
    My previous solutions already implemented this

---
+++ servers/Exercises/07/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go serve(conn)
+	}
+
+}
+
+func serve(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Fprintf(conn, "%s\n", ln)
+		if ln == "" {
+			break
+		}
+	}
+
+	defer conn.Close()
+	fmt.Println("Code got here")
+	io.WriteString(conn, "I see you connected")
+}
